964aedc7365bcf991ed51b319c6d9128f92da6c2
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-05-31 04:44:05 -0300

    numbers with input

---
+++ compare.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = get_int ("Digite um valor para x: ");
+    int y = get_int ("Digite um valor para y: ");
+
+    if ( x > y)
+    {
+        printf("X é maior\n");
+    }
+    else if (y > x)
+    {
+        printf("y é maior");
+    }
+    else
+    {
+        printf("Iguais");
+    }
+}
