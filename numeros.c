83ec60a99de477c023ed26f5440330b949cbc2a9
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-05-31 04:43:13 -0300

    numbers in C with condicionales

---
+++ numeros.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+    int x=5;
+    int y=2;
+    if (x>y)
+    {
+        printf("x é maior que y\n");
+    }
+    else if(x==y)
+    {
+        printf("x é igual a y");
+    }
+    else
+    {
+        printf("n não é maior que y\n");
+    }
+}
