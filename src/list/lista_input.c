e0343cb60e078ce4723ee6d11c2cec1c21f7abaf
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-06-22 00:14:49 -0300

    feat: list_input

---
+++ src/list/lista_input.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+    int tamanho;
+    do
+    {
+        tamanho = get_int("Quantos elementos voce quer na lista (max 10)? ");
+    }
+    while (tamanho <= 0 || tamanho > 10);
+
+    int lista_do_usuario[tamanho]; 
+    
+    printf("\n--- Preenchendo a lista com valores do usuario ---\n");
+    for (int i = 0; i < tamanho; i++)
+    {
+        lista_do_usuario[i] = get_int("Digite o elemento %d: ", i + 1);
+    }
+    printf("\n--- Sua lista---\n");
+    printf("[");
+    for (int i = 0; i < tamanho; i++)
+    {
+        printf("%d", lista_do_usuario[i]); 
+
+        if (i < tamanho - 1)
+        {
+            printf(", ");
+        }
+    }
+    printf("]\n"); 
+}
+
+ 
