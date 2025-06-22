4e36204ad5812efb834fdad01988c0bc4ed5c63d
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-06-22 01:15:24 -0300

    feat: soma entre intervalos

---
+++ src/for_intro/for_soma.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n = get_int("Digite um número: ");
+    int soma = 0;
+    for(int i=1;i<=n;i++)
+    {
+        soma=soma+i;
+    }
+    printf("A soma de 1 ate %d e: %d\n", n, soma);
+
+    return 0;
+}
