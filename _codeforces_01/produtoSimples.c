ee4127e377c393565d90f00f951bb664acd8fea5
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-07-12 18:11:10 -0300

    feat: add produtoSimples.c for calculating the product of two integers

---
+++ _codeforces_01/produtoSimples.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+int main(void)
+{
+    int a = get_int("Digite um valor para a:");
+    int b = get_int("Digite um valor para b:");
+    int z = a*b;
+    printf("O resultado da multiplicação é: %i \n", z);
+}
+
+
