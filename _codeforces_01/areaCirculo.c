ebc78064ad76d7bb7345bb71aa5722a0c29dcd53
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-07-12 18:10:49 -0300

    feat: add areaCirculo.c for calculating the area of a circle

---
+++ _codeforces_01/areaCirculo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+    double raio = get_double("Digite o valor do raio: ");
+    int expoente = 2;
+    double circ = 3.14159;
+    double raio_resul = pow(raio,expoente);
+    printf("%f\n" ,raio_resul * circ);
+}
+
