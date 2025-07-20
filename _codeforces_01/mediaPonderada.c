89c0f7ddd6c126a4818d956374529993cad8d7cc
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-07-20 00:16:32 -0300

    feat: question media ponderada

---
+++ _codeforces_01/mediaPonderada.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    double nota1 = get_double("nota 1:");
+    double nota2 = get_double("nota 2:");
+    int peso1 = get_int("peso 1: ");
+    int peso2 = get_int("peso 2: ");
+
+    double media = (nota1*peso1 + nota2*peso2)/(peso1+peso2);
+
+    printf("A média ponderada é: %.2f\n", media);
+    
+}
