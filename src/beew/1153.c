be9638992507265096d757b5fae59aec83a33a07
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-11-05 22:21:42 -0300

    feat: adiciona fatorial question

---
+++ src/beew/1153.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int fatorial(int n);
+int main() {
+    int n;
+    scanf("%d",&n);
+    printf("%d\n",fatorial(n));
+    return 0;
+}
+
+int fatorial(int n){
+    if(n ==1 || n == 0){
+        return 1;
+    } else{
+        return n * fatorial(n-1);
+    }
+}
